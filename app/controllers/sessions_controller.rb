eb0fd66309a5ccdf2f68f584e73c8f9fd3aa6df7
Shawn Ren <shawnren527@gmail.com>
2019-05-07 15:14:24 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(name: user_params[:name])
-    if @user.try(:authenticate, user_params[:password])
+    @user = User.find_by(name: params[:name])
+    if @user.try(:authenticate, params[:password])
       redirect_to "/users/#welcome"
       session[:user_id] = @user.id
     else
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :password, :id)
-  end
+  # private
+  # def user_params
+  #   params.require(:user).permit(:name, :password, :id)
+  # end
 end
