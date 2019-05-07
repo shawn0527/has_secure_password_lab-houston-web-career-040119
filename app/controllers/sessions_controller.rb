7dd40d79acf68f5e8a5905dcfefe406255b87c72
Shawn Ren <shawnren527@gmail.com>
2019-05-07 15:16:11 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(name: params[:name])
-    if @user.try(:authenticate, params[:password])
+    @user = User.find_by(name: user_params[:name])
+    if @user.try(:authenticate, user_params[:password])
       redirect_to "/users/#welcome"
-      session[:user_id] = user_params[:id]
+      session[:user_id] = @user.id
     else
       redirect_to '/users#new'
     end
